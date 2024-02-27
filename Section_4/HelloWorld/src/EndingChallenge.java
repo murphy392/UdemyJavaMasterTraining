2fb6638709e44f7e4b095f0afd261ba0b82ab80e
Craig Murphy <craigmurphy40@gmail.com>
2024-02-27 17:14:29 -0500

    Created section5 folder

--- Section_4/HelloWorld/src/EndingChallenge.java
+++ Section_4/HelloWorld/src/EndingChallenge.java
@@ -2,10 +2,12 @@ public class EndingChallenge {
     public static void main(String[] args) {
         double firstValue = 20.00d;
         double secondValue = 80.00d;
-        double total = (firstValue + secondValue) * 100.00;
-        double remainder = total % 40.00;
+        //putting firstValue + secondValue in parenthesis creates NO REMAINDER
+        double total = firstValue + secondValue * 100.00;
+        double remainder = total % 40.00d;
+        System.out.println("Remainder = " + remainder);
         boolean remainderExists = (remainder == 0) ? true : false;
-        System.out.println("Remainder value " + remainderExists);
+        System.out.println("Remainder exists? " + remainderExists);
 
         if (!remainderExists) {
             System.out.println("Got some remainder");
