a84003bab2a760e174afa6669dca3174468e352f
Craig Murphy <craigmurphy40@gmail.com>
2024-02-27 18:07:54 -0500

    updating progress

---
+++ section5/KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //what are 100 miles in kilometers. The below expression evaluates that
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if (highScore > 25) {
+            highScore = 1000 + highScore; // add bonus points
+        }
+
+        //expressions include assignments and evaluations. They do not include the keywords or semicolons
+        int health = 100;
+        if ((health < 25) && (highScore > 1000)) {
+            highScore = highScore - 1000;
+        }
+    }
+}
